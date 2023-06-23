d62f18c5d373ad46ca74de27dc9f5f1afe948c6e
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2023-06-24 00:36:44 +0800

    just use 3.5 turbo

--- transcribe.py
+++ transcribe.py
@@ -97,7 +97,7 @@ def qa_file(filepath):
         db = Chroma.from_documents(texts, embeddings)
         retriever = db.as_retriever(search_type = "similarity", search_kwargs = {"k":5})
         st.write("retrieval chain")
-        chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.5,model = 'gpt-3.5-turbo-16k', openai_api_key=openai_api_key),
+        chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.5,model = 'gpt-3.5-turbo', openai_api_key=openai_api_key),
                                                                             retriever=retriever)
         st.session_state.chained = True
 
