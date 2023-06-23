7f99b1728b88a1bd714e2f018b976329cab1a597
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2023-06-23 17:38:17 +0800

    download button

--- transcribe.py
+++ transcribe.py
@@ -154,6 +154,8 @@ def main():
             with st.expander("See Transcript"):
                 st.text_area("Transcript", transcript, height=200)
             write_file(transcript, "output.txt")
+            with open('output.txt') as f:
+                st.download_button('Download txt file for future use', f)  # Defaults to 'text/plain'
             qa_file("output.txt")
         except Exception as e :
             st.exception(f"An error occurred: {e}")
